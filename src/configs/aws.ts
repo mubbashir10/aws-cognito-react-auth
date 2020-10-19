d46497a34c897e605730b6364c6bc77e5ed4c3e8
mubbashir mustafa <mubbashir10@gmail.com>
2020-10-19 05:41:43 +0500

    chores: removed default values from configs

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: "us-east-2",
-  USER_POOL_ID: "us-east-2_A8sLnOI1Q",
-  APP_CLIENT_ID: "5qousdv6mvfi5g0hgvkdmc5g9l",
+  REGION: "",
+  USER_POOL_ID: "",
+  APP_CLIENT_ID: "",
 };
